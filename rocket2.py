f0a6a6e7572d98dec639f5956fa464c6a327a013
LordBOSS42 <37372273+LordBOSS42@users.noreply.github.com>
2018-03-15 09:49:45 -0400

    Update rocket2.py

--- rocket2.py
+++ rocket2.py
@@ -1,1 +1,15 @@
+from ggrocket import Rocket, Planet 
+from math import radians, sqrt
 
+earth = Planet(viewscale=0.00005)
+
+Re = 6.371E6
+Me = 5.972E24
+G = 6.674E-11
+
+Ve=sqrt (2*Me*G/Re)
+print ("Predicted escape velocity is ", Ve, " m/s")
+
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve)
+
+earth.run (rocket)
